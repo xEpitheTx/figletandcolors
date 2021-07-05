a9f8b826334738fd640c51c7faed62efff6d6e97
Spencer Krause <spencerk@skyward.com>
2021-07-05 09:01:35 -0500

    init commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const figlet = require("figlet");
+const colors = require("colors");
+figlet('Ascii Ski', function(error, data){
+    if(error){
+        console.log('Error');
+        console.dir(error);
+        return;
+    }
+    console.log(data.rainbow);
+})
